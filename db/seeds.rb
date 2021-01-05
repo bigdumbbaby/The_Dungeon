80e7aa82b3d12091a81122868851ef298c9c5f0f
Colton <colton.shawn.oconnor@gmail.com>
2021-01-05 11:03:54 -0700

    structuring game

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,24 @@ Location.destroy_all
 tavern = Location.create name: "Tavern"
 farm = Location.create name: "Farm"
 castle = Location.create name: "Castle"
+dungeon = Location.create name: "Dungeon"
 
-ben = Npc.create name: "Ben Hen", character_class: "Wizard", ally: 0, location_id: tavern.id 
-anne = Npc.create name: "Anne Fan", character_class: "Archer", ally: 0, location_id: tavern.id
-lucas = Npc.create name: "lucas mucas", character_class: "Thief", ally: 0, location_id: farm.id
-jackson = Npc.create name: "jackson macson", character_class: "Knight", ally: 0, location_id: castle.id
+ulric = Npc.create name: "Ulric", character_class: "Wizard", ally: 0, location_id: tavern.id 
+mantagu = Npc.create name: "Lord Mantagu", character_class: "Archer", ally: 0, location_id: tavern.id
+borin = Npc.create name: "Borin", character_class: "Bard", ally: 0, location_id: tavern.id
+merek = Npc.create name: "Merek", character_class: "Thief", ally: 0, location_id: farm.id
+henry = Npc.create name: "Henry", character_class: "Farmer", ally: 0, location_id: farm.id
+fenwick = Npc.create name: "Sir John Fenwick", character_class: "Knight", ally: 0, location_id: castle.id
 
-wizard_attack_1 = Attack.create name: "Fireball", max: 50, min: 30, npc_id: ben.id
-wizard_attack_2 = Attack.create name: "Lightnight", max: 40, min: 35, npc_id: ben.id
-archer_attack_1 = Attack.create name: "arror storm", max: 60, min: 20, npc_id: anne.id
-archer_attack_2 = Attack.create name: "dagger slash", max: 40, min: 39, npc_id: anne.id
-thief_attack_1 = Attack.create name: "throat slice", max: 50, min: 40, npc_id: lucas.id
-thief_attack_2 = Attack.create name: "poison", max: 80, min: 20, npc_id: lucas.id
-knight_attack_1 = Attack.create name: "Sword Dance", max: 60, min: 40, npc_id: jackson.id 
-kngiht_attack_2 = Attack.create name: "Tackle", max: 20, min: 15, npc_id: jackson.id 
+wizard_attack_1 = Attack.create name: "Fireball", max: 40, min: 30, npc_id: ulric.id
+wizard_attack_2 = Attack.create name: "Lightning", max: 60, min: 25, npc_id: ulric.id
+archer_attack_1 = Attack.create name: "Arrow Storm", max: 65, min: 20, npc_id: mantagu.id
+archer_attack_2 = Attack.create name: "Dagger Slash", max: 40, min: 39, npc_id: mantagu.id
+bard_attack_1 = Attack.create name: "Guitar Smash", max: 35, min: 35, npc_id: borin.id
+bard_attack_2 = Attack.create name: "Singing", max: 1, min: 0, npc_id: borin.id
+thief_attack_1 = Attack.create name: "Back Stab", max: 65, min: 20, npc_id: merek.id
+thief_attack_2 = Attack.create name: "Poison", max: 70, min: 10, npc_id: merek.id
+farmer_attack_1 = Attack.create name: "Pichfork", max: 20, min: 15, npc_id: henry.id
+farmer_attack_2 = Attack.create name: "Throw Torch", max: 50, min: 5, npc_id: henry.id
+knight_attack_1 = Attack.create name: "Sword Dance", max: 90, min: 20, npc_id: fenwick.id 
+kngiht_attack_2 = Attack.create name: "Tackle", max: 20, min: 5, npc_id: fenwick.id 
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,17 +4,27 @@ class Cli
   end
   def welcome
     system('clear')
-    puts "WELCOME!!!"
-    ask = prompt.yes?("Whould you like to see the NPCs")
+    puts "Welcome Adventurer"
+    puts "Your quest is to slay the dragon in the dungeon"
+    puts "However, the dragon is tough, and you will not be able to do it alone"
+    puts "So go out and find allies to heed you in the quest"
+    ask = prompt.yes?("Whould you like to continue?")
     if ask 
-      list_npcs
+      list_locations
     else 
       puts "Too bad"
       welcome
     end
   end
 
-  def list_npcs 
-    npcs = prompt.multi_select("NPCs", Npc.all.pluck(:name))
+  def list_locations
+    location = prompt.select("Where would you like to go?", Location.all)
+    list_npcs(location)
   end
+
+  def list_npcs place
+    prompt.select("Who do you want to talk to?",  Npc.all.select{ |npc| npc.location_id == place.id}.pluck(:name))
+    binding.pry
+  end
+
 end
